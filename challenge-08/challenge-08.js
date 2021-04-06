6774742c7a1932ecd75ce341130d5a80b2fc59a5
RonyMelo97 <52936094+RonyMelo97@users.noreply.github.com>
2021-04-06 03:14:58 -0300

    Update challenge-08.js

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -94,39 +94,13 @@ function calculator(operator){
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
 */
-var sum = function calculator(operator){
-    switch(operator){
-        case '+':
-            return function(x,y){
-                console.log( 'Resultado da operação: ' + x + ' ' + operator + ' ' + y + ' = ' + (x + y) )
-            } 
-            break
-        case '-':
-            return function(x,y){
-                console.log( 'Resultado da operação: ' + x + ' ' + operator + ' ' + y + ' = ' + (x - y) )
-            } 
-        case '*':
-            return function(x,y){
-                console.log( 'Resultado da operação: ' + x + ' ' + operator + ' ' + y + ' = ' + (x * y) )
-            } 
-        case '/':
-            return function(x,y){
-                console.log( 'Resultado da operação: ' + x + ' ' + operator + ' ' + y + ' = ' + (x / y) )
-            } 
-        case '%':
-            return function(x,y){
-                console.log( 'Resultado da operação: ' + x + ' ' + operator + ' ' + y + ' = ' + (x % y) )
-            } 
-        default:
-            console.log( 'Operação inválida.' )        
-    }
-}
+var sum = calculator('+');
 
 /*
 Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
 */
 
-sum('*')(5,3)
+console.log ( sum('+')(5,3) )
 //Resultado da operação: 5 * 3 = 15
 
 /*
@@ -134,10 +108,17 @@ Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
 `division` e `mod`, e atribua a elas a função `calculator`, passando o operador
 correto por parâmetro para cada uma delas.
 */
-// ?
+
+var subtration = calculation('-')
+var multiplication = calculation('*')
+var division = calculation('/')
+var mod = calculator('%')
 
 /*
 Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
 no console.
 */
-// ?
+console.log(subtration(5,2));
+console.log(multiplication(20,8));
+console.log(division(15,3));
+console.log(mod(50,10));
