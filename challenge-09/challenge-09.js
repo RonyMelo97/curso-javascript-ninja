4a961caeea5d2a2fb0b44eb5dc24de560efc770b
RonyMelo97 <52936094+RonyMelo97@users.noreply.github.com>
2021-04-08 20:27:09 -0300

    Update challenge-09.js

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -95,16 +95,12 @@
   - O retorno dessa segunda função deve ser a função de `callback` passada
   por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
   que foram passadas para a primeira função `calculator`.
-  */
-  // ?
-  /*
-  function calculator(number1,number2){
-    return function(callback){
 
-    }
-    
+function calculator(number1,number2){
+    return function(callback){
+        return callback(number1,number2)
+    };
 }
-  */
 
   /*
   Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
