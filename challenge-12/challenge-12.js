017c1b492096ed3b79eca09c05f9779dd4dd25ab
RonyMelo97 <52936094+RonyMelo97@users.noreply.github.com>
2021-05-17 18:16:27 -0300

    Update challenge-12.js

--- challenge-12/challenge-12.js
+++ challenge-12/challenge-12.js
@@ -1,127 +1,130 @@
 (function(){
-  /*
-  Envolva todo o conteúdo desse arquivo em uma IIFE.
-  */
+    /*
+    Envolva todo o conteúdo desse arquivo em uma IIFE.
+    */
+  
+    /*
+    Crie um objeto chamado `person`, com as propriedades:
+        `name`: String
+        `lastname`: String
+        `age`: Number
+    Preencha cada propriedade com os seus dados pessoais, respeitando o tipo
+    de valor para cada propriedade.
+    */
+  
+    var person = {
+        'name' : 'Rony',
+        'lastname' : 'Melo',
+        'agr' : 23,
+    }
+    person.toString()
+    
+  
+    /*
+    Mostre no console, em um array, todas as propriedades do objeto acima.
+    Não use nenhuma estrutura de repetição, nem crie o array manualmente.
+    */
 
-  /*
-  Crie um objeto chamado `person`, com as propriedades:
-      `name`: String
-      `lastname`: String
-      `age`: Number
-  Preencha cada propriedade com os seus dados pessoais, respeitando o tipo
-  de valor para cada propriedade.
-  */
+    console.log( 'Propriedades de "person": ' + Object.keys(person) ); 
+  
+    /*
+    Crie um array vazio chamado `books`.
+    */
+  
+    var books = [];
+  
+    /*
+    Adicione nesse array 3 objetos, que serão 3 livros. Cada livro deve ter a
+    seguintes propriedades:
+    `name`: String
+    `pages`: Number
+    */
 
-  var person = {
-      'name' : 'Rony',
-      'lastname' : 'Melo',
-      'agr' : 23,
-  }
+        books.push({name: 'Homem Aranha','pages': 370})
+        books.push({name: 'Pantera negra','pages': 490})
+        books.push({name: 'Homem Formiga','pages': 420})
+  
+    /*
+    Mostre no console todos os livros.
+    */
+    var text = '\nLista de livros :';
+    for(var prop in books){
+      text = text + books[prop].name + ', '
+    }
+    console.log(text)
 
-  console.log( 'Propriedades de "person":' );
+    /*
+    Remova o último livro, e mostre-o no console.
+    */
+    var removed = books.pop()
+    console.log( '\nLivro que está sendo removido:', removed  );
+  
+    
+    /*
+    Mostre no console os livros restantes.
+    */
 
-  /*
-  Mostre no console, em um array, todas as propriedades do objeto acima.
-  Não use nenhuma estrutura de repetição, nem crie o array manualmente.
-  */
-  var arr = JSON.stringify(person)
-  console.log( 'Propriedades de "person": '+ arr ); //ou
-  console.log( 'Propriedades de "person": '+ JSON.stringify(person) ); 
+    console.log( '\nAgora sobraram somente os livros:', books );
+  
+    /*
+    Converta os objetos que ficaram em `books` para strings.
+    */
 
-  /*
-  Crie um array vazio chamado `books`.
-  */
+   var bookString = JSON.stringify(books)
+    
+    /*
+    Mostre os livros nesse formato no console:
+    */
 
-  var books = []
+    console.log( '\nLivros em formato string:', bookString) ;
+  
+    /*
+    Converta os livros novamente para objeto.
+    */
+    
+    bookObj = JSON.parse(bookString)
+    console.log( '\nAgora os livros são objetos novamente:',bookObj);
+  
+    /*
+    Mostre no console todas as propriedades e valores de todos os livros,
+    no formato abaixo:
+        "[PROPRIEDADE]: [VALOR]"
+    */
+    
+    for(var i = 0; i < books.length; i++){
+        for ( var prop in books[i] ){
+            console.log(prop + ': ' + books[i][prop])
+        }
+    }
+    console.log(Object.keys( books ))
 
-  /*
-  Adicione nesse array 3 objetos, que serão 3 livros. Cada livro deve ter a
-  seguintes propriedades:
-  `name`: String
-  `pages`: Number
-  */
-  var livro1 = {
-          'name': 'Homem Aranha',
-          'pages': 370
-      }
+    /*
+    Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
+    seu nome. Adicione seu nome completo no array.
+    */
+    // ?
+    var myName = ['R','o','n','y']
+    
 
-      var livro2 = {
-          'name': 'Pantera negra',
-          'pages': 490
-      }
-
-      var livro3 = {
-          'name': 'Homem Formiga',
-          'pages': 420
-      }
-
-      books.push(livro1,livro2,livro3)
-
-  console.log( '\nLista de livros:' );
-
-  /*
-  Mostre no console todos os livros.
-  */
-  // ?
-
-  console.log( '\nLivro que está sendo removido:' );
-  /*
-  Remova o último livro, e mostre-o no console.
-  */
-  // ?
-
-  console.log( '\nAgora sobraram somente os livros:' );
-  /*
-  Mostre no console os livros restantes.
-  */
-  // ?
-
-  /*
-  Converta os objetos que ficaram em `books` para strings.
-  */
-  // ?
-  console.log( '\nLivros em formato string:' );
-
-  /*
-  Mostre os livros nesse formato no console:
-  */
-  // ?
-
-  /*
-  Converta os livros novamente para objeto.
-  */
-  // ?
-  console.log( '\nAgora os livros são objetos novamente:' );
-
-  /*
-  Mostre no console todas as propriedades e valores de todos os livros,
-  no formato abaixo:
-      "[PROPRIEDADE]: [VALOR]"
-  */
-  // ?
-
-  /*
-  Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
-  seu nome. Adicione seu nome completo no array.
-  */
-  // ?
-  console.log( '\nMeu nome é:' );
-
-  /*
-  Juntando todos os itens do array, mostre no console seu nome.
-  */
-  // ?
-
-  console.log( '\nMeu nome invertido é:' );
-
-  /*
-  Ainda usando o objeto acima, mostre no console seu nome invertido.
-  */
-  // ?
-
-  console.log( '\nAgora em ordem alfabética:' );
-  /*
-  Mostre todos os itens do array acima, odenados alfabéticamente.
-  */
-  // ?
-}())
+  
+    /*
+    Juntando todos os itens do array, mostre no console seu nome.
+    */
+    console.log( '\nMeu nome é:',);
+    console.log( myName.join('') )
+  
+    /*
+    Ainda usando o objeto acima, mostre no console seu nome invertido.
+    */
+    // ?
+    console.log( '\nMeu nome invertido é:' );
+    console.log( myName.reverse().join(''))
+    
+    /*
+    Mostre todos os itens do array acima, odenados alfabéticamente.
+    */
+    console.log( '\nAgora em ordem alfabética:' );
+    console.log(myName.sort())
+    // ?
+  }())
+  
